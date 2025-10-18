07fd369b6a50713ea81f96c9f0295e2a38fbc832
Alexandr <102229491+AxyonichA@users.noreply.github.com>
2025-10-18 20:10:25 +0500

    sql query timeouts

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"time"
 
 	"github.com/lib/pq"
 )
@@ -31,6 +32,9 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 			RETURNING id, created_at, updated_at
 	`
 
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+	
 	err := s.db.QueryRowContext(
 		ctx, 
 		query, 
@@ -57,6 +61,9 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 			FROM posts 
 			WHERE id = $1
 	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	var post Post
 
@@ -92,6 +99,9 @@ func (s *PostStore) Delete(ctx context.Context, id int64) error {
 		DELETE FROM posts WHERE id = $1
 	`
 
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
 	res, err := s.db.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
@@ -116,6 +126,9 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 		WHERE id = $3 AND version = $4
 		RETURNING version
 	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	err := s.db.QueryRowContext(
 		ctx, 
