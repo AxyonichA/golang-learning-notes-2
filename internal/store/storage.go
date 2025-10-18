82b37e3fdf37f0f9c45375bcaff255b8035f1751
Alexandr <102229491+AxyonichA@users.noreply.github.com>
2025-10-18 16:49:34 +0500

    comments, updating and deleting posts

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -14,9 +14,14 @@ type Storage struct {
 	Posts interface {
 		GetByID(context.Context, int64) (*Post, error)
 		Create(context.Context, *Post) error
+		Delete(context.Context, int64) error
+		Update(context.Context, *Post) error
 	}
 	Users interface {
 		Create(context.Context, *User) error
+	}
+	Comments interface {
+		GetByPostID(context.Context, int64) ([]Comment, error)
 	}
 }
 
@@ -24,5 +29,6 @@ func NewStorage(db *sql.DB) Storage {
 	return Storage{
 		Posts: &PostStore{db},
 		Users: &UserStore{db},
+		Comments: &CommentStore{db},
 	}
 }
