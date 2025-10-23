cc20a290be3ba306838824e8a57585c76c5113f1
Alexandr <102229491+AxyonichA@users.noreply.github.com>
2025-10-23 18:22:54 +0500

    validating tokens

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"fmt"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -29,10 +31,16 @@ func (a *JWTAuthenticator) GenerateToken(claims jwt.Claims) (string, error) {
 }
 
 func (a *JWTAuthenticator) ValidateToken(token string) (*jwt.Token, error) {
-	return jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+	return jwt.Parse(token, func(t *jwt.Token) (any, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, jwt.ErrSignatureInvalid
+			return nil, fmt.Errorf("unexpected signing method %v", t.Header["alg"])
 		}
+
 		return []byte(a.secret), nil
-	})
+	},
+		jwt.WithExpirationRequired(),
+		jwt.WithAudience(a.aud),
+		jwt.WithIssuer(a.aud),
+		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Name}),
+	)
 }
